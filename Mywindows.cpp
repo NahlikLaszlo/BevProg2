983fe8a006c90cc7659d48a5eba2c3728a9e83e6
NahlikLaszlo <38831900+NahlikLaszlo@users.noreply.github.com>
2018-05-14 19:43:44 +0200

    Delete Mywindows.cpp

--- Mywindows.cpp
+++
@@ -1,8 +0,0 @@
-#include "Mywindows.hpp"
-#include "CloseButton.h"
-#include<AveregeButton.h>
-Mywindows::Mywindows(int x,int y):Windows(x,y)
-{
-   mytank=new Tank(200,200,x/20,x/40);
-   w.push_back(mytank);
-}
